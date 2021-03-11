8c13c4fb077edaf897426f5c76bd1e2e3f3861d8
Nikita Matsnev <niki-sim@mail.ru>
2021-03-11 19:30:14 +0300

    switch on listener and finally work up prettier

--- js/test.js
+++ js/test.js
@@ -1,83 +1,83 @@
 let obj = {
   "coord":
   {
-      "lon":-0.1257,
-      "lat":51.5085
+    "lon": -0.1257,
+    "lat": 51.5085
   },
   "weather":
-  [
+    [
       {
-          "id":804,
-          "main":"Clouds",
-          "description":"overcast clouds",
-          "icon":"04n"
+        "id": 804,
+        "main": "Clouds",
+        "description": "overcast clouds",
+        "icon": "04n"
       }
-  ],
-  "base":"stations",
+    ],
+  "base": "stations",
   "main":
   {
-      "temp":282.09,
-      "feelslike":276.85,
-      "tempmin":280.93,
-      "temp_max":283.15,
-      "pressure":1018,
-      "humidity":53
+    "temp": 282.09,
+    "feelslike": 276.85,
+    "tempmin": 280.93,
+    "temp_max": 283.15,
+    "pressure": 1018,
+    "humidity": 53
   },
-  "visibility":10000,
+  "visibility": 10000,
   "wind":
   {
-      "speed":4.63,
-      "deg":230
+    "speed": 4.63,
+    "deg": 230
   },
   "clouds":
   {
-      "all":100
+    "all": 100
   },
-  "dt":1615318418,
+  "dt": 1615318418,
   "sys":
   {
-      "type":1,
-      "id":1414,
-      "country":"GB",
-      "sunrise":1615271283,
-      "sunset":1615312453
+    "type": 1,
+    "id": 1414,
+    "country": "GB",
+    "sunrise": 1615271283,
+    "sunset": 1615312453
   },
-  "timezone":0,
-  "id":2643743,
-  "name":"London",
-  "cod":200
+  "timezone": 0,
+  "id": 2643743,
+  "name": "London",
+  "cod": 200
 }
 
 let citiesObj = [
   {
-      "id": 833,
-      "name": "Ḩeşār-e Sefīd",
-      "state": "",
-      "country": "IR",
-      "coord": {
-          "lon": 47.159401,
-          "lat": 34.330502
-      }
+    "id": 833,
+    "name": "Ḩeşār-e Sefīd",
+    "state": "",
+    "country": "IR",
+    "coord": {
+      "lon": 47.159401,
+      "lat": 34.330502
+    }
   },
   {
-      "id": 2960,
-      "name": "‘Ayn Ḩalāqīm",
-      "state": "",
-      "country": "SY",
-      "coord": {
-          "lon": 36.321911,
-          "lat": 34.940079
-      }
+    "id": 2960,
+    "name": "‘Ayn Ḩalāqīm",
+    "state": "",
+    "country": "SY",
+    "coord": {
+      "lon": 36.321911,
+      "lat": 34.940079
+    }
   },
   {
-      "id": 3245,
-      "name": "Taglag",
-      "state": "",
-      "country": "IR",
-      "coord": {
-          "lon": 44.98333,
-          "lat": 38.450001
-      }
+    "id": 3245,
+    "name": "Taglag",
+    "state": "",
+    "country": "IR",
+    "coord": {
+      "lon": 44.98333,
+      "lat": 38.450001
+    }
   }
 ]
 
@@ -86,8 +86,8 @@ const getCurrentPosition = () => {
   navigator.geolocation.getCurrentPosition(success, error, {
     enableHighAccuracy: true
   })
-  
-  function success({coords}) {
+
+  function success({ coords }) {
     reqAPI(undefined, coords, undefined)
   }
 
@@ -96,30 +96,31 @@ const getCurrentPosition = () => {
   }
 
 }
-  const reqAPI = (name, coords, id) => {
-let url = 'https://community-open-weather-map.p.rapidapi.com/weather?'
-if (!name) {
-  url += `lat=${coords.latitude}&lon=${coords.longitude}`
-}
-else {
-  url += `q=${name}`
-}
 
-    fetch(url, {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "cadf04a247msh09c8c1c6bee7505p1a566ajsn5c1a379faca1",
-        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-      },
-        })
-        .then(resp => {return resp.text()})
-        .then(resBody => {
-          updateHtmlData(JSON.parse(resBody), id)
-        })
-        .catch(err => {
-          console.error(err);
-        });
+const reqAPI = (name, coords, id) => {
+  let url = 'https://community-open-weather-map.p.rapidapi.com/weather?'
+  if (!name) {
+    url += `lat=${coords.latitude}&lon=${coords.longitude}`
   }
+  else {
+    url += `q=${name}`
+  }
+
+  fetch(url, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "cadf04a247msh09c8c1c6bee7505p1a566ajsn5c1a379faca1",
+      "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+    },
+  })
+    .then(resp => { return resp.text() })
+    .then(resBody => {
+      updateHtmlData(JSON.parse(resBody), id)
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
 
 const updateHtmlData = (data = obj, id = "current") => {
   let city = document.getElementById(id);
@@ -129,7 +130,7 @@ const updateHtmlData = (data = obj, id = "current") => {
   city.querySelector('.cloud').innerHTML = numToStringCloud(data.clouds.all)
   city.querySelector('.pressure').innerHTML = `${data.main.pressure} hpa`
   city.querySelector('.humidity').innerHTML = `${data.main.humidity} %`
-  city.querySelector('.coord').innerHTML = `[${data.coord.lat}, ${data.coord.lon}]` 
+  city.querySelector('.coord').innerHTML = `[${data.coord.lat}, ${data.coord.lon}]`
 }
 
 function degToCompass(num) {
@@ -150,17 +151,17 @@ const delFavoriteCity = (id) => {
 }
 
 const addFavoriteCity = () => {
-let cityName = addCityForm.cityName.value
-let data = obj
+  let cityName = addCityForm.cityName.value
+  let data = obj
 
-let countCityCard = document.querySelectorAll(".weather-city").length
-while (document.getElementById(`fav-city-${countCityCard}`)) {
-  countCityCard++
-}
+  let countCityCard = document.querySelectorAll(".weather-city").length
+  while (document.getElementById(`fav-city-${countCityCard}`)) {
+    countCityCard++
+  }
 
-localStorage.setItem(countCityCard, cityName)
-createFavoriteCityCard(countCityCard)
-reqAPI(cityName, undefined, `fav-city-${countCityCard}`)
+  localStorage.setItem(countCityCard, cityName)
+  createFavoriteCityCard(countCityCard)
+  reqAPI(cityName, undefined, `fav-city-${countCityCard}`)
 }
 
 const createFavoriteCityCard = (countCityCard) => {
@@ -201,16 +202,15 @@ const createFavoriteCityCard = (countCityCard) => {
   </ul>
 </div>`
 
-parent.insertAdjacentHTML("beforeEnd", cityCard)
+  parent.insertAdjacentHTML("beforeEnd", cityCard)
 }
 
-function showFavorites() {
-    let keys = Object.keys(localStorage)
 
-        for(let i = 0; i < keys.length; i++) {
-          createFavoriteCityCard(keys[i]);
-          reqAPI(localStorage.getItem(keys[i]), undefined, `fav-city-${keys[i]}`)
-        }
-}
+document.addEventListener("DOMContentLoaded", () => {
+  let keys = Object.keys(localStorage)
 
-showFavorites();
+  for (let i = 0; i < keys.length; i++) {
+    createFavoriteCityCard(keys[i]);
+    reqAPI(localStorage.getItem(keys[i]), undefined, `fav-city-${keys[i]}`)
+  }
+});
